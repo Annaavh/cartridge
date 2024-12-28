4676286c61e96bf0c6a94a80e16639b53070b0a0
Anna <annaavetisyan175@gmail.com>
2024-12-28 23:11:08 +0400

    save changes

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
         <footer className="footer">
             <div className="container">
                 <div className="social-icons">
-                    <i class="fa-brands fa-facebook"></i>
-                    <a href="https://www.facebook.com/CartridgeaM099575752" target="_blank">Cartridge.aM</a>
+                    <a href="https://www.facebook.com/CartridgeaM099575752" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="https://www.facebook.com/CartridgeaM099575752" target="_blank">Cartridge</a>
                 </div>
                 <div className="contact-info">
                     <span className="phone-number">Զանգահարեք՝</span>
